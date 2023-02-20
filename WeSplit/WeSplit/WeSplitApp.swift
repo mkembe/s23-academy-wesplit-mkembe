02e34f5e4500a5843cf9172fcd187e06f7506338
Millie Kembe <mikembe@unc.edu>
2023-02-20 16:39:33 -0500

    WeSplit Project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Millie Kembe on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
